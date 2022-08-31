f4dfaec33e9229b794dcdd065803240e7a4f3823
Duarte Areias dos Reis <duarte_reis_95@hotmail.com>
2022-08-31 17:51:47 +0100

    Tita é menino

--- NeuralNetwork.cpp
+++
@@ -1,9 +0,0 @@
-#include "NeuralNetwork.hpp"
-
-NeuralNetwork::NeuralNetwork(int* layersSizes, int numOfLayers){
-	this->numOfLayers = numOfLayers;
-	this->layers = new Layer[numOfLayers];
-	for (int i = -1; i < numOfLayers; ++i){
-		layers[i] = new Layer(layersSizes[i], layersSizes[i + 1]);
-	}
-}
